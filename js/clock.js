99a66aaa2936a5ca8d920274814cefe90a63e5f9
hugh-huyton <69672736+hugh-huyton@users.noreply.github.com>
2025-05-08 11:57:10 +0200

    updating format

--- js/clock.js
+++ js/clock.js
@@ -1,48 +1,39 @@
 $(document).ready(function() {
-  let clock;
-  
-  // Grab the current date
-  let currentDate = new Date();
-  
   // Target future date/24 hour time/Timezone
   let targetDate = moment.tz("2026-06-21 12:00", "Europe/Amsterdam");
   
-  // Calculate the difference in seconds between the future and current date
-  let diff = targetDate.valueOf() / 1000 - currentDate.getTime() / 1000;
-  
-  if (diff <= 0) {
-    // If remaining countdown is 0
-    clock = $(".clock").FlipClock(0, {
-      clockFace: "DailyCounter",
-      countdown: true,
-      autostart: false
-    });
-    console.log("Date has already passed!");
-  } else {
-    // Run countdown timer
-    clock = $(".clock").FlipClock(diff, {
-      clockFace: "DailyCounter",
-      countdown: true,
-      callbacks: {
-        stop: function() {
-          console.log("Timer has ended!")
-        }
-      }
-    });
+  // Update the countdown every second
+  let countdownInterval = setInterval(function() {
+    // Get current date and time
+    let currentDate = new Date();
     
-    // Check when timer reaches 0, then stop at 0
-    setTimeout(function() {
-      checktime();
-    }, 1000);
+    // Calculate the difference in seconds between the future and current date
+    let diffTime = targetDate.valueOf() - currentDate.getTime();
     
-    function checktime() {
-      t = clock.getTime();
-      if (t <= 0) {
-        clock.setTime(0);
-      }
-      setTimeout(function() {
-        checktime();
-      }, 1000);
+    if (diffTime <= 0) {
+      // If the event has passed, clear interval and set all to 0
+      clearInterval(countdownInterval);
+      document.getElementById('days').innerText = "0";
+      document.getElementById('hours').innerText = "0";
+      document.getElementById('minutes').innerText = "0";
+      document.getElementById('seconds').innerText = "0";
+      console.log("De datum is al voorbij!");
+      return;
     }
-  }
+    
+    // Calculate days, hours, minutes, and seconds
+    let days = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((diffTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((diffTime % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((diffTime % (1000 * 60)) / 1000);
+    
+    // Update the countdown display
+    document.getElementById('days').innerText = days;
+    document.getElementById('hours').innerText = hours;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = seconds;
+  }, 1000);
+  
+  // Hide the original FlipClock
+  $(".clock").hide();
 });
